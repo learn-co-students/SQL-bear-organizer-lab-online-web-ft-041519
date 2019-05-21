84f0bce77a422ab34921a15739762dfe6bf94580
Erin Stalzer <erinstalzer@gmail.com>
2019-05-21 15:21:49 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears(id)
+-- VALUES (1),
+-- (2),
+-- (3),
+-- (4),
+-- (5),
+-- (6),
+-- (7);
+
+INSERT INTO bears(name)
+VALUES (NULL, "Ted"),
+("Ted"),
+("Ted"),
+("Ted"),
+("Ted"),
+("Ted"),
+("Ted");
